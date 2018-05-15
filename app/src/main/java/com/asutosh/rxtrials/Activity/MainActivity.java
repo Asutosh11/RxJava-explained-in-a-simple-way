599f68d0c4208a42bf7c8328546a596848d3e06c
Asutosh Panda <panda.asutosh04@gmail.com>
2018-05-15 10:11:49 +0530

    remove dynamic buttons

--- app/src/main/java/com/asutosh/rxtrials/Activity/MainActivity.java
+++ app/src/main/java/com/asutosh/rxtrials/Activity/MainActivity.java
@@ -11,31 +11,13 @@ import com.asutosh.rxtrials.R;
 
 public class MainActivity extends AppCompatActivity{
 
-    LinearLayout LL_root;
-    String[] activityNames =
-            {"Basic: simplest RxJava example",
-             "Basic: emit & Receive Objects Array",
-             "Basic: emit & Receive Objects List",
-            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LL_root = findViewById(R.id.container);
-
-        for(int j=0;j<activityNames.length;j++) {
-
-            Button button = new Button(this);
-            button.setText(activityNames[j]);
-            LL_root.addView(button);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            lp.setMargins(2, 2, 2, 2);
-            lp.gravity = Gravity.CENTER;
-            button.setLayoutParams(lp);
-            button.setBackgroundResource(R.color.lightIndigo);
-        }
-
     }
+
+    
 }
